daf79520f450a6a7aa7b452c2fe73befdfbfee43
Daniel George <danieljamesgeorge@gmail.com>
2020-06-22 16:36:50 +0100

    section 2

--- math.php
+++ math.php
@@ -7,7 +7,21 @@
     <title>Document</title>
 </head>
 <body>
-        
+      <?php 
+      
+      //can do maths as normal 
+      echo 55+33;
+      echo "<br>";
+      echo 55-33;
+      echo "<br>";
+      echo 55*33;
+      echo "<br>";
+      echo 55/33;
+      echo "<br>";
+
+      //parentheses govern order of operation 
+      
+      ?>  
 
 
 </body>
